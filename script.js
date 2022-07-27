8a444aa6baf7954a5523fd05e6803d68bda3df1c
isaacGabrielle <domino.isaacgabrielle.bscs2021@gmail.com>
2022-07-26 23:10:41 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+function aboutMe(){
+    location.href="aboutMe.html";
+}
+function contact(){
+    location.href="contact.html";
+}
+function photography(){
+    location.href="photography.html";
+}
+function projects(){
+    location.href="project.html";
+}
